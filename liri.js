ed8c003e5c82f3b97d5062bdd3bd9c68981e455d
B312 <11902197+davidscott321@users.noreply.github.com>
2019-02-17 03:20:54 -0600

    LIRI Application Complete. README Needed

--- liri.js
+++ liri.js
@@ -8,12 +8,14 @@ var spotify = new Spotify(keys.spotify);
 
 var command = process.argv[2];
 var originalInput = process.argv.slice(3).join(" ");
-var parameter = process.argv.slice(3).join("+").trim();
+var parameter = process.argv.slice(3).join("+");
+var logText;
 
 if(parameter)
 {
   if(command==="concert-this")
   {
+    // console.log("\n");
     findConcerts(parameter);
   }
   else if(command==="spotify-this-song")
@@ -22,32 +24,45 @@ if(parameter)
   }
   else if(command==="movie-this")
   {
-    // Search for a movie in the OMDB database.
+    findMovie(parameter);
   }
   else if(command==="do-what-it-says")
   {
-    // Random search based on commands in the random.txt file.
+    if(parameter)
+    {
+      logText = "\nThe do-what-it-says command doesn't require a parameter so I removed all of the text after the command.\n";
+      appendLogText();
+
+      console.log(logText);
+    }
+    randomOriginalContent();
   }
   else
   {
-    console.log("You did not type in a valid command for LIRI. Please try again.\n");
-    console.log("There are four different commands for LIRI.\n");
-    console.log("1) node liri.js concert-this <insert artist name>\n");
-    console.log("2) node liri.js spotify-this-song <insert song name with or without artist name>\n");
-    console.log("3) node liri.js movie-this <insert movie name>\n");
-    console.log("4) node liri.js do-what-it-says\n");
-    console.log("Here is an example of how to use LIRI: 'node liri.js movie-this The Matrix'\n");
+    logText = "\nYou did not type in a valid command for LIRI. Please try again.\n\n";
+    logText+="There are four different commands for LIRI.\n\n";
+    logText+="1) node liri.js concert-this <insert artist name>\n";
+    logText+="2) node liri.js spotify-this-song <insert song name with or without artist name>\n";
+    logText+="3) node liri.js movie-this <insert movie name>\n";
+    logText+="4) node liri.js do-what-it-says\n\n";
+    logText+="Here is an example of how to use LIRI: 'node liri.js movie-this The Matrix'\n";
+    appendLogText();
+
+    console.log(logText);
   }
 }
 else if(!command)
 {
-  console.log("You did not type in a valid command for LIRI. Please try again.\n");
-  console.log("There are four different commands for LIRI.\n");
-  console.log("1) node liri.js concert-this <insert artist name>\n");
-  console.log("2) node liri.js spotify-this-song <insert song name with or without artist name>\n");
-  console.log("3) node liri.js movie-this <insert movie name>\n");
-  console.log("4) node liri.js do-what-it-says\n");
-  console.log("Here is an example of how to use LIRI: 'node liri.js movie-this The Matrix'\n");
+  logText = "\nYou did not type in a valid command for LIRI. Please try again.\n\n";
+  logText+="There are four different commands for LIRI.\n\n";
+  logText+="1) node liri.js concert-this <insert artist name>\n";
+  logText+="2) node liri.js spotify-this-song <insert song name with or without artist name>\n";
+  logText+="3) node liri.js movie-this <insert movie name>\n";
+  logText+="4) node liri.js do-what-it-says\n\n";
+  logText+="Here is an example of how to use LIRI: 'node liri.js movie-this The Matrix'\n";
+  appendLogText();
+
+  console.log(logText);
 }
 else
 {
@@ -61,7 +76,10 @@ else
     originalInput = originalInputArray[1].trim();
     parameter = originalInput.split(" ").join("+");
 
-    console.log(`You aren't specifying an artist with the concert-this command. It's okay, I have selected this artist randomly for you: ${originalInput}.\n`);
+    logText = `\nYou aren't specifying an artist with the concert-this command. It's okay, I have selected this artist randomly for you: ${originalInput}.\n`;
+    appendLogText();
+
+    console.log(logText);
 
     findConcerts(parameter);
   }
@@ -70,28 +88,41 @@ else
     originalInput = "The Sign Ace of Base";
     parameter = originalInput.split(" ").join("+");
 
-    console.log(`You aren't specifying a song with the spotify-this-song command. It's okay, I have selected this song for you: ${originalInput}.\n`);
+    logText = `\nYou aren't specifying a song with the spotify-this-song command. It's okay, I have selected this song for you: ${originalInput}.\n`;
+    appendLogText();
+
+    console.log(logText);
 
     findSong(parameter);
   }
   else if(command==="movie-this")
   {
-    // Search for a movie in the OMDB database.
-    parameter = "Mr. Nobody";
+    originalInput = "Mr. Nobody";
+    parameter = originalInput.split(" ").join("+");
+
+    logText = `\nYou aren't specifying a movie with the movie-this command. It's okay, I have selected this movie for you: ${originalInput}.\n`;
+    appendLogText();
+
+    console.log(logText);
+
+    findMovie(parameter);
   }
   else if(command==="do-what-it-says")
   {
-    // Random search based on commands in the random.txt file.
+    randomOriginalContent();
   }
   else
   {
-    console.log("You did not type in a valid command for LIRI. Please try again.\n");
-    console.log("There are four different commands for LIRI.\n");
-    console.log("1) node liri.js concert-this <insert artist name>\n");
-    console.log("2) node liri.js spotify-this-song <insert song name with or without artist name>\n");
-    console.log("3) node liri.js movie-this <insert movie name>\n");
-    console.log("4) node liri.js do-what-it-says\n");
-    console.log("Here is an example of how to use LIRI: 'node liri.js movie-this The Matrix'\n");
+    logText = "\nYou did not type in a valid command for LIRI. Please try again.\n\n";
+    logText+="There are four different commands for LIRI.\n\n";
+    logText+="1) node liri.js concert-this <insert artist name>\n";
+    logText+="2) node liri.js spotify-this-song <insert song name with or without artist name>\n";
+    logText+="3) node liri.js movie-this <insert movie name>\n";
+    logText+="4) node liri.js do-what-it-says\n\n";
+    logText+="Here is an example of how to use LIRI: 'node liri.js movie-this The Matrix'\n";
+    appendLogText();
+
+    console.log(logText);
   }
 }
 
@@ -102,7 +133,10 @@ function findConcerts(artistName)
   axios.get(URL).then(function(response) {
     var jsonData = response.data;
 
-    console.log(`Here are the next three concerts for ${originalInput}.\n`);
+    logText = `Here are the next three concerts for ${originalInput}.\n`;
+    appendLogText();
+
+    console.log(logText);
 
     for(var i=0;i<3;i++)
     {
@@ -110,50 +144,62 @@ function findConcerts(artistName)
 
       if(jsonData[i].venue.region != '')
       {
-        console.log(`
-          Concert ${i+1} Details:
-          Venue: ${jsonData[i].venue.name}.
-          Location: ${jsonData[i].venue.city}, ${jsonData[i].venue.region}.
-          Country: ${jsonData[i].venue.country}.
-          Date and Time: ${timeStamp[0]} at ${timeStamp[1]}.
-        `);
+        logText = `Concert ${i+1} Details:\n`;
+        logText+=`Venue: ${jsonData[i].venue.name}.\n`;
+        logText+=`Location: ${jsonData[i].venue.city}, ${jsonData[i].venue.region}.\n`;
+        logText+=`Country: ${jsonData[i].venue.country}.\n`;
+        logText+=`Date and Time: ${timeStamp[0]} at ${timeStamp[1]}.\n`;
+        appendLogText();
+
+        console.log(logText);
       }
       else
       {
-        console.log(`
-          Concert ${i+1} Details:
-          Venue: ${jsonData[i].venue.name}.
-          City: ${jsonData[i].venue.city}.
-          Country: ${jsonData[i].venue.country}.
-          Date and Time: ${timeStamp[0]} at ${timeStamp[1]}.
-        `);
+        logText = `Concert ${i+1} Details:\n`;
+        logText+=`Venue: ${jsonData[i].venue.name}.\n`;
+        logText+=`City: ${jsonData[i].venue.city}.\n`;
+        logText+=`Country: ${jsonData[i].venue.country}.\n`;
+        logText+=`Date and Time: ${timeStamp[0]} at ${timeStamp[1]}.\n`;
+        appendLogText();
+
+        console.log(logText);
       }
     }
   }).catch(function(error){
-    console.log("\nThere was an error with this search result from the Bands in Town API. Here is the error:\n");
-    console.log(error);
-    console.log("\nCan you try using the concert-this command with a different artist?\n");
+    logText = "\nThere was an error with this search result from the Bands in Town API. Here is the error:\n";
+    logText+=error;
+    logText+="\nCan you try using the concert-this command with a different artist?\n";
+    appendLogText();
+
+    console.log(logText);
   });
 }
 
 function findSong(songName) 
 {
-  console.log(`Here is the song search result for ${originalInput}.`);
+  logText = `Here is the song search result for ${originalInput}.\n`;
+  appendLogText();
+
+  console.log(logText);
 
   spotify.search({
     type: 'track',
     query: songName,
     limit: 1
-  },function (err, data) {
-    if(err) 
+  },function (error, data) {
+    if(error) 
     {
-      console.log("\nThere was an error with this search result from the Spotify API. Here is the error:\n");
-      console.log(err);
-      console.log("\nCan you try using the spotify-this-song command with a different song?\n");
+      logText = "\nThere was an error with this search result from the Spotify API. Here is the error:\n";
+      logText+=error;
+      logText+="\nCan you try using the spotify-this-song command with a different song?\n";
+      appendLogText();
+
+      console.log(logText);
     }
     else
     {
       var artists = [];
+      var previewLink;
 
       //for loop to populate the artists array in case there is more than one artist.
       for(var i=0;i<data.tracks.items[0].artists.length;i++)
@@ -161,17 +207,105 @@ function findSong(songName)
         artists.push(data.tracks.items[0].artists[i].name);
       }
 
-      console.log(`
-        Artist(s): ${artists.join(", ")}
-        Song Name: ${data.tracks.items[0].name}
-        Spotify Preview Link: ${data.tracks.items[0].preview_url}
-        Album Name: ${data.tracks.items[0].album.name}
-      `);
+      if(!data.tracks.items[0].preview_url)
+      {
+        previewLink = "No Preview link found on Spotify.";
+      }
+      else
+      {
+        previewLink = data.tracks.items[0].preview_url;
+      }
+
+      logText = `Artist(s): ${artists.join(", ")}\n`;
+      logText+=`Song Name: ${data.tracks.items[0].name}\n`;
+      logText+=`Spotify Preview Link: ${previewLink}\n`;
+      logText+=`Album Name: ${data.tracks.items[0].album.name}\n`;
+      appendLogText();
+
+      console.log(logText);
     }
   })
 }
-    // Append showData and the divider to log.txt, print showData to the console
-    // fs.appendFile("log.txt", showData + divider, function(err) {
-    //   if (err) throw err;
-     
-    // });
+
+function findMovie(movieName)
+{
+  var URL = "http://www.omdbapi.com/?t="+movieName+"&apikey=trilogy";
+
+  axios.get(URL).then(function(response) {
+    var jsonData = response.data;
+    var rottenTomatoesRating;
+    
+    // for loop to find the Rotten Tomatoes key and then save the correct rating.
+    for(var i=0;i<jsonData.Ratings.length;i++)
+    {
+      if(jsonData.Ratings[i].Source==="Rotten Tomatoes")
+      {
+        rottenTomatoesRating=jsonData.Ratings[i].Value;
+      }
+    }
+    if(!rottenTomatoesRating)
+    {
+      rottenTomatoesRating="No Rotten Tomatoes Rating Found in the OMDB data.";
+    }
+
+    logText = `Here is the movie search result for ${originalInput}.\n\n`;
+    logText+=`Title: ${jsonData.Title}\n`;
+    logText+=`Year: ${jsonData.Year}\n`;
+    logText+=`IMDB Rating: ${jsonData.imdbRating}\n`;
+    logText+=`Rotten Tomatoes Rating: ${rottenTomatoesRating}\n`;
+    logText+=`Production Country: ${jsonData.Country}\n`;
+    logText+=`Language(s): ${jsonData.Language}\n`;
+    logText+=`Plot: ${jsonData.Plot}\n`;
+    logText+=`Actors: ${jsonData.Actors}\n`;
+    appendLogText();
+
+    console.log(logText);
+  }).catch(function(error){
+    logText = "\nThere was an error with this search result from the OMDB API. Here is the error:\n";
+    logText+=error;
+    logText+="\nCan you try using the movie-this command with a different movie?\n";
+    appendLogText();
+
+    console.log(logText);
+  });
+}
+
+function randomOriginalContent()
+{
+  var lineNumber = Math.floor(Math.random() * 7);
+  var data = fs.readFileSync("./random.txt","utf8");
+  var lines = data.split("\n");
+  var originalInputArray = lines[lineNumber].split('"');
+
+  originalInput = originalInputArray[1].trim();
+  parameter = originalInput.split(" ").join("+");
+
+  if(originalInputArray[0]==="spotify-this-song,")
+  {
+    logText = `\nHere is your randomly generated command 'node liri.js spotify-this-song ${originalInput}'\n`;
+    appendLogText();
+
+    console.log(logText);
+
+    findSong(parameter);
+  }
+  else
+  {
+    logText = `\nHere is your randomly generated command 'node liri.js concert-this ${originalInput}'\n`;
+    appendLogText();
+
+    console.log(logText);
+
+    findConcerts(parameter);
+  }
+}
+
+function appendLogText()
+{
+  var divider = "\n-----------------------------------------\n";
+  // Append showData and the divider to log.txt, print showData to the console
+  fs.appendFile("log.txt",divider+logText,function(error) {
+    if (error) throw error;
+    
+  });
+}
